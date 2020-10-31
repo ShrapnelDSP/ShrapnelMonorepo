24d27659d118097008a88bb36dd012a6143c7fb8
Barabas Raffai <barabas.raffai@gmail.com>
2020-10-31 17:36:41 +0000

    Added web UI to control gain

---
+++ components/esp_http_websocket_server/src/httpd_websocket.c
@@ -0,0 +1,268 @@
+#include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_httpd_priv.h"
+
+#define TAG "websocket"
+
+//the max size of output websocket frames in bytes this is the max size we can
+//support right now, as we only use one byte to store the size
+#define WEBSOCKET_BUFFER_SIZE 127
+
+typedef struct {
+    bool fin;
+    uint8_t rsv123;
+    enum {
+        CONTINUATION_FRAME,
+        TEXT_FRAME,
+        BINARY_FRAME,
+        CONNECTION_CLOSE_FRAME = 8,
+        PING_FRAME,
+        PONG_FRAME
+    } opcode;
+    bool mask;
+    uint64_t payload_len;
+    char *mask_key; /*<! Pointer to the first octet of the mask key */
+    char *payload;  /*<! Pointer to the first octet of the payload  */
+} websocket_frame_data_t;
+
+
+static QueueHandle_t in_queue;
+
+esp_err_t httpd_websocket_init(QueueHandle_t q)
+{
+    in_queue = q;
+    return ESP_OK;
+}
+
+/* build binary data from websocket frame data struct */
+static esp_err_t httpd_websocket_build(const websocket_frame_data_t *d, char *buf, size_t buf_len, size_t *output_size)
+{
+    if(d->mask)
+    {
+        ESP_LOGE(TAG, "Masking is not supported");
+        return ESP_FAIL;
+    }
+
+    if(d->payload_len > 125)
+    {
+        ESP_LOGE(TAG, "Payload length over 125 is not supported");
+        return ESP_FAIL;
+    }
+
+    //Payload plus 2 framing bytes must fit into the output buffer.
+    //There can be more framing bytes if the payload size is larger than 125,
+    //or if masking is enabled. However, neither of these is supported for now.
+    if(buf_len < d->payload_len + 2)
+    {
+        ESP_LOGE(TAG, "Output buffer is too small");
+        return ESP_FAIL;
+    }
+
+    memset(buf, 0, buf_len);
+
+    buf[0] |= d->fin    << 7;
+    buf[0] |= d->rsv123 << 4;
+    buf[0] |= d->opcode;
+
+    buf[1] |= d->mask << 7;
+    buf[1] |= d->payload_len;
+    
+    memcpy(&buf[2], d->payload, d->payload_len);
+
+    ESP_LOGD(TAG, "websocket frame build finished");
+    ESP_LOG_BUFFER_HEXDUMP(TAG, buf, buf_len, ESP_LOG_DEBUG);
+
+    *output_size = 2 + d->payload_len;
+    return ESP_OK;
+}
+
+esp_err_t httpd_websocket_send_string(int fd, char *s)
+{
+    char buf[WEBSOCKET_BUFFER_SIZE];
+    size_t size;
+    ssize_t sock_size;
+
+    websocket_frame_data_t d = {
+        .fin = true,
+        .opcode = TEXT_FRAME,
+        .mask = false,
+    };
+
+    d.payload_len = strlen(s);
+    d.payload = s;
+
+    esp_err_t ret = httpd_websocket_build(&d, buf, WEBSOCKET_BUFFER_SIZE, &size);
+    if(ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "websocket frame build failed");
+        return ret;
+    }
+
+    sock_size = write(fd, buf, size);
+    if(sock_size != size)
+    {
+        ESP_LOGE(TAG, "Socket error %d: %s", errno, strerror(errno));
+        return ESP_FAIL;
+    }
+
+    return ESP_OK;
+}
+
+/* parse the data from a websocket frame and print it to the screen */
+esp_err_t httpd_websocket_parse(httpd_handle_t hd, char *buf, size_t buf_len, int fd)
+{
+    esp_err_t err = ESP_OK;
+    websocket_frame_data_t data = {0};
+
+    //offset due to larger payload size
+    int offset;
+    uint32_t tmp;
+
+    data.fin          = (buf[0] >> 7) & 0x01;
+    data.rsv123       = (buf[0] >> 4) & 0x07;
+    data.opcode       = (buf[0])      & 0x0f;
+
+    data.mask         = (buf[1] >> 7) & 0x01;
+
+    ESP_LOGD(TAG, "fin:\t0x%02x", data.fin);
+    ESP_LOGD(TAG, "rsv123:\t0x%02x", data.rsv123);
+    ESP_LOGD(TAG, "opcode:\t0x%02x", data.opcode);
+    ESP_LOGD(TAG, "mask:\t0x%02x", data.mask);
+    if(!data.mask)
+    {
+        ESP_LOGE(TAG, "Client should always mask");
+        return ESP_FAIL;
+    }
+
+    tmp               = (buf[1])      & 0x7f;
+    ESP_LOGD(TAG, "tmp_len:\t0x%02x", tmp);
+
+    if(tmp == 126)
+    {
+        //the next two bytes are the extended length
+        offset = 2;
+    }
+    else if(tmp == 127)
+    {
+        //the next 8 bytes are the extended length
+        offset = 8;
+    }
+    else
+    {
+        //the extended length was not used
+        data.payload_len = tmp;
+        offset = 0;
+    }
+
+    for(int i = offset; i > 0; i--)
+    {
+        //TODO is this correct? Maybe use 64-bit ntoh if available
+        ESP_LOGW(TAG, "Payload size was multi-byte, result might be wrong");
+        data.payload_len |= buf[2 + i];
+        data.payload_len <<= 8;
+    }
+    ESP_LOGD(TAG, "pl_len:\t0x%08llx", data.payload_len);
+        
+    data.mask_key = &buf[2 + offset];
+    for(int i = 0; i < 4; i++)
+    {
+        ESP_LOGD(TAG, "key[%d]:\t0x%02x", i, data.mask_key[i]);
+    }
+
+    data.payload = &buf[6 + offset];
+
+    switch(data.opcode)
+    {
+        case CONTINUATION_FRAME:
+            ESP_LOGW(TAG, "Got continuation frame");
+            break;
+        case TEXT_FRAME:
+            ESP_LOGI(TAG, "Got text data frame");
+
+            /* send a copy of the payload to the input queue */
+            char *copy = malloc(data.payload_len + 1);//+1 for the null terminator
+            if(copy == NULL)
+            {
+                ESP_LOGE(TAG, "Couldn't malloc");
+                return ESP_FAIL;
+            }
+
+            for(int i = 0; i < data.payload_len; i++)
+            {
+                char c;
+                c = data.payload[i] ^ data.mask_key[i%4];
+                ESP_LOGD(TAG, "dump: %c", c);
+
+                if(c == '\0')
+                {
+                    ESP_LOGW(TAG, "Found NULL terminator in payload");
+                }
+                copy[i] = c;
+            }
+            copy[data.payload_len] = '\0';
+
+            BaseType_t ret = xQueueSend(in_queue, &copy, pdMS_TO_TICKS(100));
+
+            if(ret != pdPASS)
+            {
+                ESP_LOGE(TAG, "Output queue error, message dropped: %s", copy);
+                free(copy);
+                err = ESP_FAIL;
+            }
+            break;
+        case BINARY_FRAME:
+            ESP_LOGW(TAG, "Got binary data frame");
+            break;
+        case CONNECTION_CLOSE_FRAME:
+            ESP_LOGW(TAG, "Got connection close frame");
+            //TODO cleanly close the session associated with fd
+            int n = shutdown(fd, SHUT_WR);
+            if(n != 0)
+                ESP_LOGE(TAG, "shutdown returned error %d, %s", errno, strerror(errno));
+
+            do 
+            {
+                n = recv(fd, buf, buf_len, 0);
+                ESP_LOGD(TAG, "recv returned %d", n);
+                if(n < 0)
+                    ESP_LOGE(TAG, "recv error %d, %s", errno, strerror(errno));
+            } while (n != 0);
+
+            n = close(fd);
+            if(n != 0)
+                ESP_LOGE(TAG, "close returned error %d, %s", errno, strerror(errno));
+
+            break;
+        case PING_FRAME:
+            ESP_LOGW(TAG, "Got ping frame");
+            break;
+        case PONG_FRAME:
+            ESP_LOGW(TAG, "Got pong frame");
+            break;
+        default:
+            ESP_LOGE(TAG, "Got unknown opcode: %xh", data.opcode);
+            break;
+    }
+
+    return err;
+}
+
+esp_err_t httpd_websocket_print_payload(websocket_frame_data_t *data)
+{
+    if(data->payload_len > 100)
+    {
+        ESP_LOGE(TAG, "payload length is too high, aborting (%lld)", data->payload_len);
+        return ESP_FAIL;
+    }
+
+    for(int i = 0; i < data->payload_len; i++)
+    {
+        ESP_LOGD(TAG, "dump: %c", data->payload[i] ^ data->mask_key[i%4]);
+    }
+    return ESP_OK;
+}
--- main/main.c
+++ main/main.c
@@ -1,35 +1,245 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_log.h"
 #define TAG "main"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "esp32/sha.h"
+#include "mbedtls/base64.h"
+#include "protocol_examples_common.h"
+
 #include "i2s.h"
+#include "esp_http_websocket_server.h"
+
+//this magic is used during the websocket handshake
+const char WEBSOCKET_KEY_MAGIC[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+#define STRLEN(a) (sizeof(a)/sizeof(char))
+
+#define QUEUE_LEN 10
+QueueHandle_t in_queue;
+QueueHandle_t out_queue;
+
+static esp_err_t websocket_get_handler(httpd_req_t *req)
+{
+    const size_t buf_len = 100;
+    size_t len;
+    char   buf[buf_len];
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (len > 1) {
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+    }
+
+    len = httpd_req_get_hdr_value_len(req, "Upgrade") + 1;
+    if (len > 1) {
+        if (httpd_req_get_hdr_value_str(req, "Upgrade", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Upgrade: %s", buf);
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Upgrade header error");
+        }
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Websocket request contained no Upgrade header");
+    }
+
+    len = httpd_req_get_hdr_value_len(req, "Connection") + 1;
+    if (len > 1) {
+        if (httpd_req_get_hdr_value_str(req, "Connection", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Connection: %s", buf);
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Connection header error");
+        }
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Websocket request contained no Connection header");
+    }
+
+    len = httpd_req_get_hdr_value_len(req, "Sec-WebSocket-Key") + 1;
+    if (len > 1) {
+        if (httpd_req_get_hdr_value_str(req, "Sec-WebSocket-Key", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Sec-WebSocket-Key: %s", buf);
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Sec-WebSocket-Key header error");
+        }
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Websocket request contained no Sec-WebSocket-Key header");
+    }
+
+    //concatenate the magic to the request's key
+    strcat(buf, WEBSOCKET_KEY_MAGIC);
+
+    //calculate the sha1 of this
+    //sha1 is always 20 bytes long
+    unsigned char sha1[20];
+    esp_sha(SHA1, (unsigned char *)buf, strlen(buf), sha1);
+
+    //base64 encode the sha1
+    size_t olen;
+    mbedtls_base64_encode((unsigned char *)buf, buf_len - 1, &olen, sha1, 20);
+    ESP_LOGI(TAG, "Base64 encoded key length: %d", olen);
+
+    /* Response */
+    httpd_resp_set_hdr(req, "Upgrade", "websocket");
+    httpd_resp_set_hdr(req, "Connection", "Upgrade");
+    httpd_resp_set_hdr(req, "Sec-WebSocket-Accept", buf);
+    httpd_resp_set_status(req, "101 Switching Protocols");
+    httpd_resp_send(req, NULL, 0);
+
+    return ESP_OK;
+}
+
+static const httpd_uri_t websocket = {
+    .uri       = "/websocket",
+    .method    = HTTP_GET,
+    .handler   = websocket_get_handler,
+    .user_ctx  = NULL
+};
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    in_queue  = xQueueCreate(QUEUE_LEN, sizeof(char*));
+    if(in_queue == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to create in queue");
+        return NULL;
+    }
+
+    out_queue = xQueueCreate(QUEUE_LEN, sizeof(char*));
+    if(out_queue == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to create out queue");
+        return NULL;
+    }
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config, in_queue, out_queue) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &websocket);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+static void stop_webserver(httpd_handle_t server)
+{
+    // Stop the httpd server
+    httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base, 
+                               int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server) {
+        ESP_LOGI(TAG, "Stopping webserver");
+        stop_webserver(*server);
+        *server = NULL;
+    }
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base, 
+                            int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server == NULL) {
+        ESP_LOGI(TAG, "Starting webserver");
+        *server = start_webserver();
+    }
+}
 
 static void i2s_gain_task(void *param)
 {
-    bool b = 0;
-    int att;
+    char *s;
+    int gain_db;
+    int ret;
     float gain;
     while(1)
     {
-        b = !b;
-        att = b ? 0 : 6;
-        gain = powf(10.f, -att / 20.f);
-        ESP_LOGI(TAG, "attenuation is: %d dB", att);
-        ESP_LOGI(TAG, "gain is: %01.3f", gain);
+        ret = xQueueReceive(in_queue, &s, portMAX_DELAY);
+        if(ret == pdTRUE)
+        {
+            ESP_LOGI(TAG, "received websocket message: %s", s);
 
-        i2s_set_gain(gain);
-        vTaskDelay(2000/portTICK_PERIOD_MS);
+            gain_db = atoi(s);
+            gain = powf(10.f, gain_db / 20.f);
+            ESP_LOGI(TAG, "gain is: %d dB, %01.3f", gain_db, gain);
+
+            i2s_set_gain(gain);
+
+            //this should be the last step, as s will be deallocated on the
+            //other side of this queue
+            ret = xQueueSend(out_queue, &s, 100/portTICK_PERIOD_MS);
+            if(ret != pdTRUE)
+            {
+                ESP_LOGE(TAG, "Queue failed to send");
+            }
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Queue failed to receive");
+        }
     }
 }
 
 void app_main(void)
 {
     int ret;
+    static httpd_handle_t server = NULL;
+
     i2s_setup();
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+     * and re-start it upon connection.
+     */
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+
+    /* Start the server for the first time */
+    server = start_webserver();
+
 
     ret = xTaskCreate(i2s_gain_task, "i2s gain", 2000, NULL, 5, NULL);
     if(ret != pdPASS)
@@ -37,5 +247,5 @@ void app_main(void)
         ESP_LOGE(TAG, "Gain task create failed %d", ret);
     }
 
-    printf("setup done\n");
+    ESP_LOGI(TAG, "setup done");
 }
