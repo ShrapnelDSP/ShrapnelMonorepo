0a37584412b340907999f85db134576dfc3a0e32
Barabas Raffai <barabas.raffai@gmail.com>
2021-04-02 22:38:52 +0100

    Optimise floating point conversion

--- components/float_convert/float_convert.h
+++ components/float_convert/float_convert.h
@@ -1,6 +1,15 @@
 #include <stdint.h>
+#include <math.h>
 
-static inline int32_t float_to_int32(float f)
+#define INT24_MAX 8388607
+
+static int32_t float_to_int32(float f)
 {
-    return (int32_t) (f * INT32_MAX);
+    f *= INT24_MAX;
+    if(f > 0.f) {
+        return (int32_t)(f + 0.5f) << 8;
+    }
+    else {
+        return (int32_t)(f - 0.5f) << 8;
+    }
 }
