ce4fcd47f887c87ef495f70cd26d7cc47ca40d08
Barabas Raffai <barabas.raffai@gmail.com>
2021-11-18 21:05:03 +0000

    Merging vibrato to master

---
+++ plugins/vibrato/PluginProcessor.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include "abstract_dsp.h"
+
+//==============================================================================
+class AudioPluginAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    AudioPluginAudioProcessor();
+    ~AudioPluginAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    using AudioProcessor::processBlock;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    juce::AudioProcessorValueTreeState parameters;
+
+    std::atomic<float> *modulationRateHzParameter = nullptr;
+    std::atomic<float> *modulationDepthNormalisedParameter = nullptr;
+
+    float sampleRate = 0;
+
+    dspal_delayline_t delayline;
+
+    float phase;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
+};
