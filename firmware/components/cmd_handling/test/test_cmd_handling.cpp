d309133574f8247beb9891d5d5b6a0d699f39cb1
Barabas Raffai <barabas.raffai@gmail.com>
2021-12-12 11:32:32 +0000

    try to add a new test executable to component
    Don't work, all esp-idf objects are getting linked to it. Should try
    making a standlone project for tests, using some variable to make the
    components behave as a test or as an esp-idf component.

--- firmware/components/cmd_handling/test/test_cmd_handling.cpp
+++ firmware/components/cmd_handling/test/test_cmd_handling.cpp
@@ -1,7 +1,15 @@
 /* cJSON source code is used, it is not mocked
  * esp_log is replaced with a fake that prints to stdout
  */
+#include "cJSON.h"
+#include <stdio.h>
+//#include "CppUTest.h"
 
 /* Queue returns fail, param_update_parameter is not called */
 /* Queue returns an invalid message, param_update_parameter is not called */
 /* Queue returns a valid message, param_update_parameter is called with correct parameters */
+int main(void)
+{
+    printf("hello world");
+    return 0;
+}
