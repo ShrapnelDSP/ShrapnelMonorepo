ed64c8c0a6fc4cde7d9aaec1a4ae6b0b8bfbfb25
Barabas Raffai <barabas.raffai@gmail.com>
2021-12-19 22:55:32 +0000

    Fix linker error from incorrect usage of class template

--- firmware/components/queue/include/queue.h
+++ firmware/components/queue/include/queue.h
@@ -2,6 +2,7 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include <cassert>
 
 namespace shrapnel {
 
@@ -22,12 +23,24 @@ template <typename T>
 class Queue: public QueueBase<T>
 {
     public:
-    Queue(int number_of_elements);
-    BaseType_t receive(T *out, TickType_t time_to_wait) override;
-    BaseType_t send(T *out, TickType_t time_to_wait) override;
+    Queue(int number_of_elements) : QueueBase<T>(number_of_elements)
+    {
+        handle = xQueueCreate(number_of_elements, sizeof(T));
+        assert(handle);
+    }
+
+    BaseType_t receive(T *out, TickType_t time_to_wait) override
+    {
+        return xQueueReceive(handle, out, time_to_wait);
+    }
+
+    BaseType_t send(T *out, TickType_t time_to_wait) override
+    {
+        return xQueueSendToBack(handle, out, time_to_wait);
+    }
 
     private:
     QueueHandle_t handle;
 };
 
-};
+}
