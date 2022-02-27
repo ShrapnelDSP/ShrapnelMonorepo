930403bf4057d6084ecaa472a2b3e9ad1bf59b09
Barabas Raffai <barabas.raffai@gmail.com>
2022-02-27 14:59:47 +0000

    Add flutter test that includes all files

---
+++ frontend/test/all_test.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_import
+
+/* This file includes all other files in the project, so that untested files
+ * show up propely in test coverage.
+ */
+
+import 'package:shrapnel/amplifier.dart';
+import 'package:shrapnel/chorus.dart';
+import 'package:shrapnel/heavy_metal.dart';
+import 'package:shrapnel/knob.dart';
+import 'package:shrapnel/main.dart';
+import 'package:shrapnel/noise_gate.dart';
+import 'package:shrapnel/parameter.dart';
+import 'package:shrapnel/pedalboard.dart';
+import 'package:shrapnel/stompbox.dart';
+import 'package:shrapnel/tube_screamer.dart';
+import 'package:shrapnel/valvestate.dart';
+
+void main() {}
