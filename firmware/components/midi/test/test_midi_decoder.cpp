b1501d997c9d8bcaeedba520e916462e4af17077
Barabas Raffai <barabas.raffai@gmail.com>
2022-07-21 20:46:14 +0100

    Implement MIDI channel decoding

--- firmware/components/midi/test/test_midi_decoder.cpp
+++ firmware/components/midi/test/test_midi_decoder.cpp
@@ -42,6 +42,7 @@ TEST_F(MidiDecoder, NoteOn)
 {
     Message expected{
         .type{NOTE_ON},
+        .channel{1},
         .note_on{
             .note{0},
             .velocity{1},
@@ -65,6 +66,7 @@ TEST_F(MidiDecoder, NoteOff)
 {
     Message expected{
         .type{NOTE_OFF},
+        .channel{1},
         .note_off{
             .note{0},
             .velocity{1},
@@ -88,6 +90,7 @@ TEST_F(MidiDecoder, ProgramChange)
 {
     Message expected{
         .type{PROGRAM_CHANGE},
+        .channel{1},
         .program_change{
             .number{0},
         },
@@ -109,6 +112,7 @@ TEST_F(MidiDecoder, NoteOnRunningStatus)
 {
     Message expected_first{
         .type{NOTE_ON},
+        .channel{1},
         .note_on{
             .note{0},
             .velocity{1},
@@ -117,6 +121,7 @@ TEST_F(MidiDecoder, NoteOnRunningStatus)
 
     Message expected_second{
         .type{NOTE_ON},
+        .channel{1},
         .note_on{
             .note{2},
             .velocity{3},
@@ -145,6 +150,7 @@ TEST_F(MidiDecoder, ControlChange)
 {
     Message expected{
         .type{CONTROL_CHANGE},
+        .channel{1},
         .control_change{
             .control{0},
             .value{1},
@@ -182,10 +188,35 @@ TEST_F(MidiDecoder, ChannelModeIsNotControlChange)
     }
 }
 
+TEST_F(MidiDecoder, ChannelNumber)
+{
+    Message expected{
+        .type{NOTE_ON},
+        .channel{16},
+        .note_on{
+            .note{0},
+            .velocity{1},
+        },
+    };
+
+    EXPECT_CALL(receiver, Call(MessageMatches(expected))).Times(1);
+
+    std::vector<uint8_t> bytes {
+        0x9F,
+        0x00,
+        0x01,
+    };
+
+    for (auto byte : bytes) {
+        sut.decode(byte);
+    }
+}
+
 // TODO System exclusive message does not upset decoder
 // TODO Unimplemented commands do not upset decoder (Pitch bend change, 0xE0 0x00
 // TODO Bank select:
 //      Control Change 0x00 followed by Control change 0x20, finally followed
 //      by program change is used to select a program from a 14-bit bank
 //      address.
-
+// TODO Real-Time messages may be sent at any time — even between bytes of a
+//      message which has a different status.
