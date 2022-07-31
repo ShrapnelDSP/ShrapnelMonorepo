758ce590c60204d279b030a7aee71342354fed13
Barabas Raffai <barabas.raffai@gmail.com>
2022-07-31 09:27:00 +0100

    Refactor heap tracing to allow non-scoped usage

--- firmware/components/heap_tracing/include/heap.h
+++ firmware/components/heap_tracing/include/heap.h
@@ -11,6 +11,17 @@ class EspHeapTracing {
         return instance;
     }
 
+    void start(void)
+    {
+        ESP_ERROR_CHECK(heap_trace_start(HEAP_TRACE_ALL));
+    }
+
+    void stop(void)
+    {
+        ESP_ERROR_CHECK(heap_trace_stop());
+        heap_trace_dump();
+    }
+
     private:
     EspHeapTracing() {
         ESP_ERROR_CHECK(heap_trace_init_standalone(record_buffer, N));
@@ -19,21 +30,20 @@ class EspHeapTracing {
     heap_trace_record_t record_buffer[N];
 };
 
+
+
 template<std::size_t N>
 class ScopedHeapTracing {
     public:
     ScopedHeapTracing()
     {
-        // make sure heap tracing has been initialised. This is a singleton, so
-        // initialisation will only run once
-        EspHeapTracing<N>::instance();
-
-        ESP_ERROR_CHECK(heap_trace_start(HEAP_TRACE_ALL));
+        auto tracing = EspHeapTracing<N>::instance();
+        tracing.start();
     }
 
     ~ScopedHeapTracing()
     {
-        ESP_ERROR_CHECK(heap_trace_stop());
-        heap_trace_dump();
+        auto tracing = EspHeapTracing<N>::instance();
+        tracing.stop();
     }
 };
