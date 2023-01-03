39cbeb6cff52ebda169910fe4f0cbcc166f77bfa
Barabas Raffai <barabas.raffai@gmail.com>
2023-01-03 20:57:10 +0000

    Update all test file

--- frontend/test/all_test.dart
+++ frontend/test/all_test.dart
@@ -24,15 +24,30 @@
  */
 
 import 'package:shrapnel/amplifier.dart';
+import 'package:shrapnel/audio_events.dart';
 import 'package:shrapnel/chorus.dart';
 import 'package:shrapnel/heavy_metal.dart';
+import 'package:shrapnel/json_websocket.dart';
 import 'package:shrapnel/knob.dart';
+import 'package:shrapnel/knob_with_label.dart';
 import 'package:shrapnel/main.dart';
+import 'package:shrapnel/midi_mapping/model/midi_learn.dart';
+import 'package:shrapnel/midi_mapping/model/midi_learn_state.dart';
+import 'package:shrapnel/midi_mapping/model/models.dart';
+import 'package:shrapnel/midi_mapping/model/service.dart';
+import 'package:shrapnel/midi_mapping/view/midi_learn.dart';
+import 'package:shrapnel/midi_mapping/view/midi_mapping.dart';
 import 'package:shrapnel/noise_gate.dart';
 import 'package:shrapnel/parameter.dart';
 import 'package:shrapnel/pedalboard.dart';
+import 'package:shrapnel/robust_websocket.dart';
 import 'package:shrapnel/stompbox.dart';
 import 'package:shrapnel/tube_screamer.dart';
+import 'package:shrapnel/util/conditional_parent.dart';
+import 'package:shrapnel/util/uuid.dart';
 import 'package:shrapnel/valvestate.dart';
+import 'package:shrapnel/wah.dart';
+import 'package:shrapnel/websocket_status.dart';
+import 'package:shrapnel/wifi_provisioning.dart';
 
 void main() {}
