eb508983c48d7e94cde1451347e85a8634fefed1
Barabas Raffai <barabas.raffai@gmail.com>
2024-01-21 17:10:03 +0000

    Implement UART driver

--- firmware-rust/components-renamed-to-disable-esp-idf-magic/rust-firmware-rust/src/lib.rs
+++ firmware-rust/components-renamed-to-disable-esp-idf-magic/rust-firmware-rust/src/lib.rs
@@ -1,4 +1,13 @@
 #![no_std]
+
+use esp_idf_svc::hal::prelude::Peripherals;
+use esp_idf_svc::hal::uart::config::Config as UartConfig;
+use esp_idf_svc::hal::uart::UartRxDriver;
+use esp_idf_svc::hal::units::Hertz;
+
+mod midi;
+
+
 #[no_mangle]
 extern "C" fn rust_main() -> i32 {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -8,7 +17,21 @@ extern "C" fn rust_main() -> i32 {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
-    42
+    log::info!("Rust task starting");
+
+    let peripherals = Peripherals::take().unwrap();
+    let rx = peripherals.pins.gpio26;
+
+    let config = UartConfig::new().baudrate(Hertz(31_250));
+    let uart = UartRxDriver::new(peripherals.uart1, rx, Option::<esp_idf_svc::hal::gpio::Gpio0>::None, Option::<esp_idf_svc::hal::gpio::Gpio0>::None, &config).unwrap();
+
+    loop {
+        let mut byte = [0u8; 1];
+
+        match uart.read(byte.as_mut_slice(), 10).unwrap() {
+            0 => (),
+            _ => log::info!("MIDI received byte: {:x}", byte[0]),
+        }
+    }
 }
 
