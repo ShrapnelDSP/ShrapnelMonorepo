bd58d390f2496c6e47cd43979ff2d451d55d98b3
Barabas Raffai <barabas.raffai@gmail.com>
2024-01-25 18:59:38 +0000

    Implement formatting

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 #!/bin/env bash
 
-find effects firmware plugins test -regex '.*\.\(cpp\|hpp\|c\|h\)' -not -regex ".*\(/build\|/cmake-build\|test/support\|.venv/\|firmware/managed_components\).*"
+files=$(find effects firmware plugins test -regex '.*\.\(cpp\|hpp\|c\|h\)' -not -regex ".*\(/build\|/cmake-build\|test/support\|.venv/\|firmware/managed_components\).*")
+
+clang-format -i $files
