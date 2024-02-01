1ae7b442dc297910bc1205daf57d81049ec174ef
Barabas <barabas.raffai@gmail.com>
2024-02-01 21:12:54 +0000

    Fix provisioning (#110)
    * Refactor NVS implementation to avoid UB

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -297,7 +297,7 @@ extern "C" void app_main(void)
     /* Start the mdns service */
     start_mdns();
 
-#if 1
+#if 0
     rc = xTaskCreate(profiling_task,
                      "i2s profiling",
                      2000,
