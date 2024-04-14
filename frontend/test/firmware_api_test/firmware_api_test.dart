1c9a64dad8a248aed8e466b2449a31a24406302f
Barabas Raffai <barabas.raffai@gmail.com>
2024-04-14 22:34:06 +0100

    Implement WiFi provisioning
    To use this version the following must be set up:
    
    * Create a test_config.json file at the root of the repository. This
      should be a JSON file containing a single object with keys
      DUT_IP_ADDRESS, NETWORK_SSID, NETWORK_PASSPHRASE. The network
      credentials are used to connect the DUT to the same network that the
      test runner device is on.
    
    * Pass the path to the test_config.json file using the
      "--dart-define-from-file" option.

--- frontend/test/firmware_api_test/firmware_api_test.dart
+++ frontend/test/firmware_api_test/firmware_api_test.dart
@@ -3,11 +3,15 @@ library;
 
 import 'dart:io';
 
+import 'package:esp_softap_provisioning/esp_softap_provisioning.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
 import 'package:shrapnel/api/api_websocket.dart';
 import 'package:shrapnel/core/message_transport.dart';
+import 'package:shrapnel/main.dart';
 import 'package:shrapnel/parameter.dart';
 import 'package:shrapnel/robust_websocket.dart';
+import 'package:shrapnel/wifi_provisioning.dart';
 
 // audio parameter basic test
 // factory reset
@@ -64,6 +68,8 @@ import 'package:shrapnel/robust_websocket.dart';
 
 // ignore: do_not_use_environment
 const dutIpAddress = String.fromEnvironment('DUT_IP_ADDRESS');
+const networkSsid = String.fromEnvironment('NETWORK_SSID');
+const networkPassphrase = String.fromEnvironment('NETWORK_PASSPHRASE');
 
 // Initial implementation
 //
@@ -74,6 +80,16 @@ const dutIpAddress = String.fromEnvironment('DUT_IP_ADDRESS');
 void main() {
   test('simple test', () async {
     assert(dutIpAddress.isNotEmpty, 'DUT_IP_ADDRESS must be set');
+
+    setupLogger(Level.ALL);
+
+    await eraseFlash();
+    await flashFirmware();
+    await connectToDutAccessPoint();
+    await setUpWiFi(ssid: networkSsid, password: networkPassphrase);
+
+    // Wait for firmware to start server after getting provisioned
+    await Future<void>.delayed(const Duration(seconds: 10));
 
     const uri = 'ws://$dutIpAddress:8080/websocket';
     // closed by the WebSocketTransport
@@ -116,4 +132,84 @@ class WebSocketTransportAdapter extends WebSocketTransport
   // This class is always connected. It is constructed with an already connected
   // instance of the WebSockets connection
   bool get isAlive => true;
+}
+
+Future<void> connectToDutAccessPoint() async {
+  // TODO inject the MAC address
+  final result =
+      await Process.run('nmcli', 'connection up shrapnel_17124D'.split(' '));
+
+  print(result.exitCode);
+  print(result.pid);
+  print(result.stderr);
+  print(result.stdout);
+}
+
+Future<void> setUpWiFi({required String ssid, required String password}) async {
+  final log = Logger('setUpWiFi');
+
+  log.info('setting up Wi-Fi using SSID: $ssid');
+
+  final provisioning = Provisioning(
+    security: Security1(pop: 'abcd1234'),
+    transport: TransportHTTP('guitar-dsp.local'),
+  );
+
+  var success = await provisioning.establishSession();
+  if (!success) {
+    throw StateError('provisioning failed to establish session');
+  }
+
+  success = await provisioning.sendWifiConfig(ssid: ssid, password: password);
+  if (!success) {
+    throw StateError('send wifi config failed');
+  }
+
+  success = await provisioning.applyWifiConfig();
+  if (!success) {
+    throw StateError('apply wifi config failed');
+  }
+
+  final status = await provisioning.pollStatus(const Duration(seconds: 10));
+  if (!status.isSuccess()) {
+    throw StateError('connection to provisioned network failed: $status');
+  }
+}
+
+Future<void> eraseFlash() async {
+  // pip can be used to install esptool into the global environment:
+  // https://docs.espressif.com/projects/esptool/en/latest/esp32/#quick-start
+  const command = 'esptool.py';
+  final args = ['erase_flash'];
+
+  final result = await Process.run(command, args);
+
+  print(result.exitCode);
+  print(result.pid);
+  print(result.stderr);
+  print(result.stdout);
+}
+
+Future<void> flashFirmware() async {
+  // TODO pass in path using --dart-define
+  const path = '../firmware/dist';
+
+  // pip can be used to install esptool into the global environment:
+  // https://docs.espressif.com/projects/esptool/en/latest/esp32/#quick-start
+  const command = 'esptool.py';
+  final args = '-b 2000000 '
+          '--before default_reset --after hard_reset '
+          '--chip esp32 '
+          'write_flash --flash_mode dio --flash_size 4MB --flash_freq 80m '
+          '0x1000 $path/bootloader.bin '
+          '0x8000 $path/partition-table.bin '
+          '0x10000 $path/esp32-dsp.bin'
+      .split(' ');
+
+  final result = await Process.run(command, args);
+
+  print(result.exitCode);
+  print(result.pid);
+  print(result.stderr);
+  print(result.stdout);
 }
